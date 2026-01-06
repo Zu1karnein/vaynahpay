10600d348acb2e47096ad0aaffdc147a306c82fa
Zu1karnein <jak.edmon@gmail.com>
2026-01-06 21:10:55 +0300

    deleted parthers part

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -101,189 +101,6 @@ import { Component } from '@angular/core';
       </div>
     </section>
 
-    <!-- Partners -->
-    <section id="partners" class="py-20 px-6 bg-primary-0">
-      <div class="max-w-7xl mx-auto">
-        <div class="text-center mb-16">
-          <h2 class="text-4xl lg:text-5xl font-bold text-slate-900 mb-4">
-            Партнеры
-          </h2>
-          <p class="text-xl text-slate-600">
-            Нам доверяют ведущие компании по всей стране.
-          </p>
-        </div>
-        <div class="space-y-12"></div>
-      </div>
-
-      <!-- first group -->
-      <div class="py-6">
-        <h3 class="text-2xl font-bold text-slate-900 mb-6 text-center">
-          Стратегические партнеры
-        </h3>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          <div
-            class=" p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-          >
-            <a
-              href="https://www.vtb.ru"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div
-                class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-              >
-                <img src="assets/img/VTB.png" alt="VTB" />
-              </div>
-              <h3 class="font-semibold text-slate-900">ПАО ВТБ Банк</h3>
-            </a>
-          </div>
-
-          <div
-            class="p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-          >
-            <a
-              href="https://www.sberbank.ru/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div
-                class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-              >
-                <img src="assets/img/SBER.png" alt="SBER" />
-              </div>
-              <h3 class="font-semibold text-slate-900">ПАО "СБЕРБАНК"</h3>
-            </a>
-          </div>
-
-          <div
-            class="p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-          >
-            <a
-              href="http://www.vainahtelecom.ru/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div
-                class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-              >
-                <img src="assets/img/vaynahtelecom.png" alt="vaynahtelekom" />
-              </div>
-              <h3 class="font-semibold text-slate-900">ЗАО «Вайнах Телеком»</h3>
-            </a>
-          </div>
-        </div>
-      </div>
-
-      <!-- second group -->
-      <div>
-        <h3 class="text-2xl font-bold text-slate-900 mb-6 text-center">
-          Постоянные партнеры
-        </h3>
-        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 py-3">
-          <div
-            class="p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-          >
-            <a
-              href="http://telepay.ru/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div
-                class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-              >
-                <img src="assets/img/telepay.png" alt="telepay" />
-              </div>
-              <h3 class="font-semibold text-slate-900">ООО «УЦИПС»</h3>
-            </a>
-          </div>
-
-          <div
-            class="p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-          >
-            <a
-              href="https://www.rapida.ru/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div
-                class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-              >
-                <img src="assets/img/rapida.png" alt="rapida" />
-              </div>
-              <h3 class="font-semibold text-slate-900">ООО НКО «Рапида»</h3>
-            </a>
-          </div>
-
-          <div
-            class="p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-          >
-            <a
-              href="https://www.anelik.ru/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <div
-                class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-              >
-                <img src="assets/img/anelik.png" alt="anelik" />
-              </div>
-              <h3 class="font-semibold text-slate-900">ООО КБ «Анелик»</h3>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        <div
-          class="p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-        >
-          <a
-            href="http://www.beeonline.ru/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div
-              class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-            >
-              <img src="assets/img/beeonline.png" alt="beeonline" />
-            </div>
-            <h3 class="font-semibold text-slate-900">ПАО "Вымпелком"</h3>
-          </a>
-        </div>
-        <div
-          class="p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-        >
-          <a
-            href="https://chechnya.megafon.ru/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div
-              class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-            >
-              <img src="assets/img/megafon.png" alt="megafon" />
-            </div>
-            <h3 class="font-semibold text-slate-900">ПАО "Мегафон"</h3>
-          </a>
-        </div>
-        <div
-          class="p-6 bg-white border border-slate-200 rounded-xl text-center hover:shadow-lg transition-all"
-        >
-          <a
-            href="http://webmaster95.ru"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <div
-              class="w-32 h-32 rounded-full mx-auto mb-4 flex items-center justify-center"
-            >
-              <img src="assets/img/webmaster95.png" alt="webmaster95" />
-            </div>
-            <h3 class="font-semibold text-slate-900">ООО "ВЕБМАСТЕР95"</h3>
-          </a>
-        </div>
-      </div>
-    </section>
-
     <!-- Requisites -->
     <section id="requisites" class="py-20 px-6 bg-white">
       <div class="max-w-7xl mx-auto">
@@ -299,7 +116,8 @@ import { Component } from '@angular/core';
         <div class="max-w-5xl mx-auto space-y-4">
           <!-- Полное наименование -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-slate-50 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">Полное наименование:</div>
             <div class="md:col-span-2 text-slate-700">
@@ -309,7 +127,8 @@ import { Component } from '@angular/core';
 
           <!-- Сокращенное наименование -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-white border border-slate-200 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">
               Сокращенное наименование:
@@ -319,15 +138,19 @@ import { Component } from '@angular/core';
 
           <!-- ИНН/КПП -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-slate-50 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
-            <div class="font-semibold text-slate-900">ИНН/КПП:</div>
-            <div class="md:col-span-2 text-slate-700">2005004411/201301001</div>
+            <div class="font-semibold text-slate-800">ИНН / КПП:</div>
+            <div class="md:col-span-2 text-slate-700">
+              2005004411 / 201301001
+            </div>
           </div>
 
           <!-- ОРГН -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-white border border-slate-200 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">ОРГН:</div>
             <div class="md:col-span-2 text-slate-700">1072032000534</div>
@@ -335,7 +158,8 @@ import { Component } from '@angular/core';
 
           <!-- Юридический адрес -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-slate-50 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">Юридический адрес:</div>
             <div class="md:col-span-2 text-slate-700">
@@ -346,7 +170,8 @@ import { Component } from '@angular/core';
 
           <!-- Фактический адрес -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-white border border-slate-200 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">Фактический адрес:</div>
             <div class="md:col-span-2 text-slate-700">
@@ -356,7 +181,8 @@ import { Component } from '@angular/core';
 
           <!-- Телефон, факс -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-slate-50 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">Телефон, факс:</div>
             <div class="md:col-span-2 text-slate-700">8(8712) 29-39-29</div>
@@ -364,7 +190,8 @@ import { Component } from '@angular/core';
 
           <!-- Электронная почта -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-white border border-slate-200 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">Электронная почта:</div>
             <div class="md:col-span-2">
@@ -379,7 +206,8 @@ import { Component } from '@angular/core';
 
           <!-- Сайт -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-slate-50 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">Сайт:</div>
             <div class="md:col-span-2">
@@ -396,7 +224,8 @@ import { Component } from '@angular/core';
 
           <!-- Банковские реквизиты -->
           <div
-            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6 bg-white border border-slate-200 rounded-xl"
+            class="grid grid-cols-1 md:grid-cols-3 gap-4 p-6
+         bg-emerald-100/40 border border-emerald-200 rounded-xl"
           >
             <div class="font-semibold text-slate-900">
               Банковские реквизиты:
